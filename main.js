02d707950e4f76f613af7ca5e0a696ae1cbcf8f5
Karl <karlespindola2022@gmail.com>
2022-09-20 16:52:04 -0500

    con funciones

--- main.js
+++ main.js
@@ -2,14 +2,78 @@ let pc=0;
 let yo=0;
 let ganaPc=0;
 let ganoYo=0;
-let pierdePc=0;
-let pierdoYo=0;
+
+
+
+function vs(yo,pc){
+    if(yo==1){
+        switch(pc){
+            case 1:
+                alert("Empate");
+                break;
+            case 2:
+                ganaPc++;
+      
+                alert("Pc GANA ");
+                break;
+            case 3:
+   
+                ganoYo++;
+                alert("TU GANAS ");
+                break;
+        }
+    }
+    if(yo==2){
+        switch(pc){
+            case 1:
+ 
+                ganoYo++;
+                alert("TU GANAS ");
+                break;
+            case 2:
+                alert("Empate");
+                break;    
+            case 3:
+                ganaPc++;
+
+                alert("Pc GANA ");
+                break;
+        }
+    }
+    if(yo==3){
+        switch(pc){
+            case 1:
+                ganaPc++;
+                alert("Pc GANA ");
+                break;    
+            case 2:
+                ganoYo++;
+                alert("TU GANAS ");
+                break;
+            case 3:
+                alert("Empate");
+                break;
+        }
+    }
+return array=[ganoYo,ganaPc]
+}
+
+function total(ganoYo,ganaPc){
+    if(ganoYo>ganaPc){
+        alert(`Tu ganas
+        Total partidas ganadas: ${ganoYo}`);
+    }
+    else if(ganoYo<ganaPc){
+        alert(`Pc gana;
+        Pc partidas ganadas: ${ganaPc}`)
+    }
+    else if(ganoYo==ganaPc){
+        alert(`Empate`);
+    }
+}
+
 
 let n=+prompt("Ingresa el numero de veces que jugaras");
-
-
-
-console.log(pc)
 
 for(let i=0; i<n; i++){
     yo=+prompt(`
@@ -20,67 +84,8 @@ for(let i=0; i<n; i++){
 
     pc=Math.floor(Math.random()*3+1);
 
-    if(yo==1){
-        switch(pc){
-            case 1:
-                alert("Empate");
-                break;
-            case 2:
-                ganaPc++;
-                pierdoYo++
-                alert("Pc GANA ");
-                break;
-            case 3:
-                pierdePc++;
-                ganoYo++;
-                alert("TU GANAS ");
-                break;
-        }
-    }
-    if(yo==2){
-        switch(pc){
-            case 1:
-                pierdePc++;
-                ganoYo++;
-                alert("TU GANAS ");
-                break;
-            case 2:
-                alert("Empate");
-                break;    
-            case 3:
-                ganaPc++;
-                pierdoYo++
-                alert("Pc GANA ");
-                break;
-        }
-    }
-    if(yo==3){
-        switch(pc){
-            case 1:
-                ganaPc++;
-                pierdoYo++
-                alert("Pc GANA ");
-                break;    
-            case 2:
-                pierdePc++;
-                ganoYo++;
-                alert("TU GANAS ");
-                break;
-            case 3:
-                alert("Empate");
-                break;
-        }
-    }
+    array=tvs(yo,pc);
+    total(array[0],array[1]);
 }
 
-if(ganoYo>ganaPc){
-    alert(`Tu ganas
-    Total partidas ganadas: ${ganoYo}`);
-}
-else if(ganoYo<ganaPc){
-    alert(`Pc gana;
-    Pc partidas ganadas: ${ganaPc}`)
-}
-else if(ganoYo==ganaPc){
-    alert(`Empate`);
-}
+
